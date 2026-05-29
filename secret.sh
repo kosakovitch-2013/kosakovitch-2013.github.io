3fdbeae125b4b03ba3499da38b74387be411832f
kosakovitch-2013 <kosavinna@gmail.com>
2026-05-29 23:29:58 +0300

    Update secret.sh

--- secret.sh
+++ secret.sh
@@ -1,6 +1,8 @@
 echo "you tryna to sneak huhhhh???"
 
-read -p "$ans" in
+read -p "Y/N" ans
+
+case "$ans" in
   [yY]*) echo "you sneaky sussy baka!";;
   *) echo "alright alright, i see you...";;
 esac
